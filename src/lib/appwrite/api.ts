549fc4ffb61c84e119ecb8a50a2874d16df62251
michaeljmedina22800 <michaeljmedina22800@gmail.com>
2024-02-05 19:58:52 -0500

    commit

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -5,10 +5,10 @@ import { account } from './config';
 export async function createUserAccount(user: INewUser) {
     try {
         const newAccount = await account.create(
-          ID.unique(),
-          user.email,
-          user.password,
-          user.name
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name
         )
             
         return newAccount
